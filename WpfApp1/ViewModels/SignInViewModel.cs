22694ff01b3ae81d531cd76282c000899e5ca785
Must_nu73@itstep.edu.az <Must_nu73@itstep.edu.az>
2021-09-24 16:44:19 +0400

    Change SignIn

--- WpfApp1/ViewModels/SignInViewModel.cs
+++ WpfApp1/ViewModels/SignInViewModel.cs
@@ -1,13 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using WpfApp1.Command;
 
 namespace WpfApp1.ViewModels
 {
-    public class SignInViewModel:BaseViewModel
+    public class SignInViewModel : BaseViewModel
     {
+        public SignIn SignWindow { get; set; }
+
+        public RelayCommand MouseEnterUsername { get; set; }
+
+        public RelayCommand MouseLeaveUsername { get; set; }
+        public RelayCommand MouseEnterPassword { get; set; }
+        public RelayCommand MouseLeavePassword { get; set; }
+
+        public SignInViewModel()
+        {
+            MouseEnterUsername = new RelayCommand((sender) =>
+            {
+                MouseEnter_Username();
+
+            });
+
+            MouseLeaveUsername = new RelayCommand((sender) =>
+            {
+                MouseLeave_Username();
+
+            });
+
+            MouseEnterPassword = new RelayCommand((sender) =>
+            {
+                MouseEnter_Password();
+            });
+
+            MouseLeavePassword = new RelayCommand((sender) =>
+            {
+                MouseLeave_Password();
+
+            });
+
+        }
+
+
+        public void MouseEnter_Username()
+        {
+            if (SignWindow.UsernameTxtBx.Text == "Username")
+            {
+                SignWindow.UsernameTxtBx.Text = null;
+                Color color1 = new Color();
+                color1 = Color.FromArgb(255, 37, 191, 191);
+
+                SignWindow.UsernameTxtBx.Foreground = new SolidColorBrush(color1);
+            }
+        }
+
+        public void MouseLeave_Username()
+        {
+            if (SignWindow.UsernameTxtBx.Text == "")
+            {
+
+                Color color2 = new Color();
+                color2 = Color.FromArgb(255, 110, 127, 128);
+
+                SignWindow.UsernameTxtBx.Text = "Username";
+                SignWindow.UsernameTxtBx.Foreground = new SolidColorBrush(color2);
+            }
+        }
+
+
+        public void MouseEnter_Password()
+        {
+            if (SignWindow.PasswordTxtBx.Text == "Password")
+            {
+                SignWindow.PasswordTxtBx.Text = null;
+
+
+
+                Color color1 = new Color();
+                color1 = Color.FromArgb(255, 37, 191, 191);
+
+                SignWindow.PasswordTxtBx.Foreground = new SolidColorBrush(color1);
+            }
+        }
+
+        public void MouseLeave_Password()
+        {
+            if (SignWindow.PasswordTxtBx.Text == "")
+            {
+
+                Color color2 = new Color();
+                color2 = Color.FromArgb(255, 110, 127, 128);
+
+                SignWindow.PasswordTxtBx.Text = "Password";
+                SignWindow.PasswordTxtBx.Foreground = new SolidColorBrush(color2);
+            }
+        }
+
+
 
     }
 }
--- WpfApp1/Views/SignIn.xaml.cs
+++ WpfApp1/Views/SignIn.xaml.cs
@@ -24,62 +24,9 @@ namespace WpfApp1
         public SignIn()
         {
             InitializeComponent();
-            
-        }      
-
-        private void UsernameTxtBx_MouseEnter(object sender, MouseEventArgs e)
-        {
-
-            if (UsernameTxtBx.Text == "Username")
-            {
-                UsernameTxtBx.Text = null;
-                Color color1 = new Color();
-                color1 = Color.FromArgb(255, 37, 191, 191);
-
-                UsernameTxtBx.Foreground = new SolidColorBrush(color1);
-            }
+            this.DataContext = new SignInViewModel() { SignWindow = this };
         }
 
-        private void UsernameTxtBx_MouseLeave(object sender, MouseEventArgs e)
-        {
-
-            if (UsernameTxtBx.Text == "")
-            {
-
-                Color color2 = new Color();
-                color2 = Color.FromArgb(255, 110, 127, 128);
-
-                UsernameTxtBx.Text = "Username";
-                UsernameTxtBx.Foreground = new SolidColorBrush(color2);
-            }
-        }
-
-        private void PasswordTxtBx_MouseEnter(object sender, MouseEventArgs e)
-        {
-            if (PasswordTxtBx.Text == "Password")
-            {
-                PasswordTxtBx.Text = null;
-
-
-
-                Color color1 = new Color();
-                color1 = Color.FromArgb(255, 37, 191, 191);
-
-                PasswordTxtBx.Foreground = new SolidColorBrush(color1);
-            }
-        }
-
-        private void PasswordTxtBx_MouseLeave(object sender, MouseEventArgs e)
-        {
-            if (PasswordTxtBx.Text == "")
-            {
-
-                Color color2 = new Color();
-                color2 = Color.FromArgb(255, 110, 127, 128);
-
-                PasswordTxtBx.Text = "Password";
-                PasswordTxtBx.Foreground = new SolidColorBrush(color2);
-            }
-        }
-    }
+    }    
+    
 }
